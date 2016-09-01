ab1c6ac8c5381c0c55584cfecbf128da425f047d
James Jesudason <james.jesudason@gmail.com>
2016-09-01 21:39:31 +0100

    Refactor wsgi app

---
+++ scoro2clearbooks/__init__.py
@@ -0,0 +1,31 @@
+import logging
+
+from flask import Flask
+from scoro2clearbooks.utils import run_sync
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.route('/sync')
+def run():
+    """
+    Run a sync of the Scoro to ClearBooks process
+    """
+    run_sync()
+    return 'Complete'
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
--- wsgi.py
+++ wsgi.py
@@ -1,42 +1,11 @@
 #!/usr/bin/env python
 import os
-import sys
-import logging
-
-from flask import Flask
-from scoro2clearbooks.utils import run_sync
-
-
-app = Flask(__name__)
+from scoro2clearbooks import app as application
 
 
 IP = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
 PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
-
-@app.route('/sync')
-def run():
-    """
-    Run a sync of the Scoro to ClearBooks process
-    """
-    run_sync()
-    return 'Complete'
-
-
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
-
-
 if __name__ == '__main__':
-    app.run(IP, PORT)
+    application.run(IP, PORT)
